c43b1b1dfd149c111e84c8a18acc4131b3de7c78
dylan <dylangthomas1@gmail.com>
2022-10-26 11:12:33 -0600

    first section built

--- src/App.js
+++ src/App.js
@@ -2,9 +2,53 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  const [formData, setFormData] = React.useState({
+    fname: '',
+    lname: '',
+    age: ''
+  })
+  function handleChange(event) {
+    const {name, value, type, checked} = event.target
+    setFormData(prevData => ({
+      ...formData,
+      [name]: type === "checkbox" ? checked : value
+    }))
+  }
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Form</h1>
+
+      <form>
+        <fieldset className='genInfo'>
+          <legend>General Information</legend>
+          <input 
+            type="text"
+            onChange={handleChange}
+            placeholder="First Name"
+            name="fname"
+            className='formInput'
+            value={formData.fname}
+          />
+          <input 
+            type="text"
+            onChange={handleChange}
+            placeholder="Last Name"
+            name="lname"
+            className='formInput'
+            value={formData.lname}
+          />
+          <input 
+            type="number"
+            onChange={handleChange}
+            placeholder="Age"
+            name="age"
+            className='formInput'
+            value={formData.age}
+          />
+        </fieldset>
+      </form>
     </div>
   );
 }
